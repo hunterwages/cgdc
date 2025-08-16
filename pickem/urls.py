08be94b1c5880c441b1a50d1eea5c89047eac628
Hunter Wages <hrw37@drexel.edu>
2025-08-16 15:42:20 -0400

    -b fixed weekly dues bug

--- pickem/urls.py
+++ pickem/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('standings/', views.standings, name='standings'),
     path('pick/<int:game_id>/', views.make_pick, name='make_pick'),
-    path("admin/weekly-dues/", views.weekly_dues, name="weekly_dues_latest"),
-    path("admin/weekly-dues/<int:week_id>/", views.weekly_dues, name="weekly_dues"),
+    path("weekly-dues/", views.weekly_dues, name="weekly_dues_latest"),
+    path("weekly-dues/<int:week_id>/", views.weekly_dues, name="weekly_dues"),
+
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
 ]
